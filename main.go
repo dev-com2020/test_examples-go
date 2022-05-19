376a75bb3810549078cca78f73f6f1a1c70f5526
dev-com2020 <kwp@tlen.pl>
2022-05-19 04:22:53 -0400

    czw 3

---
+++ main.go
@@ -0,0 +1,26 @@
+package testexamples
+
+/*
+  #include "hello.c"
+*/
+
+import "C"
+import (
+	"errors"
+	"log"
+)
+
+func main() {
+	err := Hello()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Hello() error {
+	_, err := C.Hello()
+	if err != nil {
+		return errors.New("blad z funkcji" + err.Error())
+	}
+	return nil
+}
